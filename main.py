8c1bc9a2c998264b74b0d473a64b8e53713c877f
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2022-05-26 16:24:55 +0000

    Add Rocket

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get('/', response_class=HTMLResponse)
-def index(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "title": "All Post"})
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get(response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request})
